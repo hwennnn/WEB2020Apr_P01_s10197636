e8ffa835d12ba58ada04e63148355fcd50f2ffc5
hwen <whman63@gmail.com>
2020-04-21 12:33:54 +0800

    practical 1.1

---
+++ web_s10197636/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web_s10197636.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
