a634637e9e22228282d87a0b7c55f37c6f8feaf1
hwen <whman63@gmail.com>
2020-05-02 23:46:52 +0800

    week 3 practical

---
+++ web_s10197636/Controllers/FineController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using web_s10197636.Models;
+
+namespace web_s10197636.Controllers
+{
+    public class FineController : Controller
+    {
+        public ActionResult Calculate()
+        {
+            //Prepare the ViewData to be use in Calculate.cshtml view
+            ViewData["ShowResult"] = false;
+            Fine fine = new Fine
+            {
+                DueDate = DateTime.Today,
+                FineRate = 0.50
+            };
+            return View(fine);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Fine fine)
+        {
+            // The fine object contains user inputs from view
+            if (!ModelState.IsValid) // validation fails
+            {
+                return View(fine); // returns the view with errors
+            }
+            // Calculate the cumulative fine and its breakdown
+            double fineTotal = 0.0;
+            string fineBreakdown = "";
+            for (int count = 1; count <= fine.NumBooksOverdue; count++)
+            {
+                double fineForEachBook = count * fine.FineRate *
+                fine.NumDaysOverdue;
+                fineTotal += fineForEachBook;
+                fineBreakdown += "Overdue cost for Book " + count + " = $" +
+                fineForEachBook.ToString("#,##0.00") +
+               "<br />";
+            }
+            fine.FineAmt = fineTotal;
+            // Prepare the ViewData to be used in Calculate.cshtml view
+            ViewData["ShowResult"] = true;
+            ViewData["FineBreakdown"] = fineBreakdown;
+            // Route to Calculate.cshtml view to display result
+            return View(fine);
+        }
+    }
+    
+}
---
+++ web_s10197636/Models/Fine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace web_s10197636.Models
+{
+    public class Fine
+    {
+        [Display(Name = "Due Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}")]
+        public DateTime DueDate { get; set; }
+
+        [Display(Name = "Number of Books Overdue")]
+        [Range(1, 10, ErrorMessage = "Invalid value! Please enter a value from 1 to 10")]
+        public int NumBooksOverdue { get; set; }
+
+        [Display(Name = "Number of Days Overdue")]
+        public int NumDaysOverdue { get; set; }
+
+        [Display(Name = "Fine Rate (SGD)")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:#,##0.00}")]
+        public double FineRate { get; set; }
+
+        [Display(Name = "Fine (SGD)")]
+        [DisplayFormat(DataFormatString = "{0:#,##0.00}")]
+        public double FineAmt { get; set; }
+
+    }
+}
